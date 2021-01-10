32e6ef59214abdda897e2409a01285e13b409b7d
salihcodev <ahmedsalihdev@gmail.com>
2021-01-11 01:42:34 +0200

    preparing the ap to start work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// UTILITIES:
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
+// ROOT COMPONENT:=>
+function CouchFurnitureDesignApp() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque modi
+        odio pariatur atque autem officia a officiis quas facere iusto veniam
+        aliquid mollitia nam quo cupiditate quos repudiandae, porro suscipit.
+      </h1>
     </div>
   );
 }
 
-export default App;
+export default CouchFurnitureDesignApp;
