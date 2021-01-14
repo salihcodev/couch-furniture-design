af9426707ee59b1159bfbe83a1faf52d30943af0
salihcodev <ahmedsalihdev@gmail.com>
2021-01-14 22:48:09 +0200

    Add & Finished subFooter component.

--- src/components/footer/sub-footer/SubFooter.comp.tsx
+++ src/components/footer/sub-footer/SubFooter.comp.tsx
@@ -2,19 +2,59 @@
 import './style.sass';
 
 // UTILITIES:
-import { NavLink } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { Container, Row, Col } from 'react-bootstrap';
 
 // IMPORT COMPONENTS:
 import { ReactComponent as Logo } from '../../../assets/couchFurnitureDesign.logo-min.svg';
-import { IoBagOutline, IoHeartOutline } from 'react-icons/io5';
 
 // INTERFACE:
 interface Props {}
 
 // COMPONENT:=>
 const SubFooter: React.FC<Props> = () => {
-  return <Container fluid>SubFooter</Container>;
+  return (
+    <Container fluid>
+      <section className="sub-footer">
+        <Row>
+          <Col>
+            <div className="rights-and-language">
+              <p className="rights-phrase">
+                @Allrights reserved to{' '}
+                <a
+                  href="https://twitter.com/salihcodev"
+                  target="_blank"
+                  rel="noreferrer noopener"
+                >
+                  couch
+                </a>
+                {'  '}
+                2020 - {new Date().getFullYear()}
+              </p>
+              <div className="switch-language">
+                <select name="select-language" id="">
+                  <option value="Select language" selected disabled>
+                    Select language
+                  </option>
+                  <option value="Arabic">Arabic</option>
+                  <option value="English">English</option>
+                </select>
+              </div>
+            </div>
+          </Col>
+          <Col>
+            <div className="company-brand">
+              <div className="logo">
+                <Link to="/">
+                  <Logo />
+                </Link>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </section>
+    </Container>
+  );
 };
 
 export default SubFooter;
